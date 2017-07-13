352c60be4f459c03d4ce06117dfa9150b8f80fb3
Mike Wight <michel.wight@hotmail.com>
2017-07-13 20:56:46 -0300

    Added addUSer in poop

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'app-crud',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrudComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+  addUser(){
+    this.userService.addUser({
+      name: "Steve",
+      nickname: "The killer"
+    });
   }
 
 }
